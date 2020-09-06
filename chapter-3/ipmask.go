032cd7d650b27f01227cde1959baafa49488b9da
Andrey Yushin <yushin.a.lightit@gmail.com>
2020-09-06 15:55:02 +0300

    [CHAPTER-3] Fix ip mask

--- chapter-3/ipmask.go
+++ chapter-3/ipmask.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	ones, bits := 24, 32
 	mask := net.CIDRMask(ones, bits)
-	ip, _ := net.ParseIP("192.168.0.1")
+	ip := net.ParseIP("192.168.0.1")
 	network := ip.Mask(mask)
 
 	fmt.Println("IP: ", ip.String())
